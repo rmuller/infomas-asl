463695baa15319961cc6250b0ec5be989dffa6f7
zc <340355960@qq.com>
2018-07-26 21:28:20 +0800

    21:28

--- annotation-detector/src/main/java/eu/infomas/annotation/AnnotationDetector.java
+++ annotation-detector/src/main/java/eu/infomas/annotation/AnnotationDetector.java
@@ -19,7 +19,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package eu.infomas.walk;
+package eu.infomas.annotation;
 
 import java.io.DataInput;
 import java.io.File;
---
+++ annotation-detector/src/main/java/eu/infomas/annotation/Zc.java
@@ -0,0 +1,5 @@
+package eu.infomas.walk;
+
+public class Zc{
+
+}

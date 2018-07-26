a77694c576ee14fb29529d6e780fc91702e36fbc
zc <340355960@qq.com>
2018-07-26 21:20:12 +0800

    21:20

--- annotation-detector/src/main/java/eu/infomas/annotation/AnnotationDetector.java
+++ annotation-detector/src/main/java/eu/infomas/annotation/AnnotationDetector.java
@@ -19,7 +19,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package eu.infomas.annotation;
+package eu.infomas.walk;
 
 import java.io.DataInput;
 import java.io.File;
